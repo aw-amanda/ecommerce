6f6efa13ac97ac30d7ba1653bf8d4e2df87246fc
min <amanda@minmachine.home>
2026-05-30 14:52:36 -0400

    deployment

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,24 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+const repoName = "ecommerce"; // Replace with your actual GitHub repository name
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  
+  basePath: isProd ? `/${repoName}` : "",
+  assetPrefix: isProd ? `/${repoName}/` : "",
+  
   images: {
+    unoptimized: true,
     qualities: [75, 85, 90],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
   },
-  reactCompiler: true,
+  
+  // GitHub Pages routing
+  trailingSlash: true,
+  
+  // Disable server components
+  distDir: "out",
 };
 
 export default nextConfig;
